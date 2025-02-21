09c10e2b96b1890327c42f76f6f18eff04dd0268
Juan Diego Bueno <juandbp@educastur.org>
2025-02-21 10:05:06 +0100

    Primera versión

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  addNota,
+  deleteNota,
+  getAllNotas,
+  getNotaById,
+  updateNota,
+} from "./notasServer";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    const obtenerNota = async (id) => {
+      const notas = await getNotaById(id);
+      console.log(notas);
+    };
+    obtenerNota(2);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
